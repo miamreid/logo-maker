ec261654631d33de90fb6985cd86b981a08d6b3a
Mia Reid <miareid@Mias-iMac.local>
2023-07-11 21:44:35 -0400

    fixed index and shape.js

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
+let shape;
+let logoText = "";
+
+// class SVG {
+//     constructor() {
+//         this.text = '',
+//         this.shape = ''
+//     }
+//     render() {
+//         return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="500" height="500">${this.shape}${this.text}</svg>`
+//     }
+//     renderText(text, color) {
+//         this.text = `<text x="250" y="250" font-size="75" text-anchor="middle" fill="${color}">${text}</text>`
+//     }
+//     renderShape(shape) {
+//         this.shape = shape.render()
+//     }
+// }
+
 const questions = [
     {
         type: "input",
@@ -18,14 +37,48 @@ const questions = [
         name: "shape",
         message: "Select a shape for the logo",
         choices: ["Circle", "Square", "Triangle"]
-    }
+    },
+    {
+        type: "input",
+        name: "fill",
+        message: "Enter the color the shape should be"
+    },
 ];
 
 function init() {
-    inquirer.prompt(questions)
-    .then((res) => {
-        const logoAnswers = new writeToFile(res);
-        logoAnswers.file();
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        switch (answers.shape) {
+            case "Triangle":
+                shape = new Triangle();
+                shape.fillColor(answers.fill);
+                logoText = shape.render();
+                break;
+            case "Square":
+                shape = new Square();
+                shape.fillColor(answers.fill);
+                logoText = shape.render();
+                break;
+            case "Circle":
+                shape = new Circle();
+                shape.fillColor(answers.fill);
+                logoText = shape.render();
+                break;
+        }
+
+        let SVG = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="500" height="500">{$shapeText}
+        <text x="250" y="250" font-size="75" text-anchor="middle" fill="${answers.color}">${answers.text}</text>
+        </svg>`;
+
+        return writeToFile("logo.svg", SVG);
+    });
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        err
+        ? console.error(err)
+        : console.log("SVG logo rendered successfully—Hooray!");
     });
 }
 
